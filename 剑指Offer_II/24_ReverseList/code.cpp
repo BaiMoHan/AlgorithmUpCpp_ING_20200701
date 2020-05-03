018a75704d4f034b826fa6caddb393d6e888cfaa
baimohan <csbaimohan@foxmail.com>
2020-05-03 19:10:20 +0800

    反转链表

---
+++ 剑指Offer_II/24_ReverseList/code.cpp
@@ -0,0 +1,21 @@
+struct  ListNode
+{
+    int m_nValue;
+    ListNode* m_pNext;
+};
+ListNode* ReverseList(ListNode *pHead)
+{
+    ListNode* pReversedHead=nullptr;
+    ListNode* pNode=pHead;
+    ListNode* pPrev=nullptr;
+    while (pNode!=nullptr)
+    {
+        ListNode* pNext=pNode->m_pNext;
+        if(pNext==nullptr)
+            pReversedHead=pNode;
+        
+        pNode->m_pNext=pPrev;
+        pNode=pNext;
+    }
+    return pReversedHead;
+}
