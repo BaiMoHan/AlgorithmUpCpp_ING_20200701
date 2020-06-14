6e272c0e866586ccceb32a616929a7f63bdd35b9
baimohan <csbaimohan@foxmail.com>
2020-06-14 16:30:58 +0800

    序列化二叉树

---
+++ 剑指Offer_II/37_SerializeBinaryTrees/code.cpp
@@ -0,0 +1,31 @@
+struct BinaryTreeNode
+{
+    int m_nValue;
+    BinaryTreeNode* m_pLeft;
+    BinaryTreeNode* m_pRight;
+};
+void Serialize(BinaryTreeNode* pRoot,ostream& stream)
+{
+    if(pRoot==nullptr)
+    {
+        stream<<"$,";
+        return;
+    }
+    stream<<pRoot->m_nValue<<',';
+    Serialize(pRoot->m_pLeft,stream);
+    Serialize(pRoot->m_pRight,stream);
+}
+void Deserialize(BinaryTreeNode** pRoot,istream& stream)
+{
+    int numver;
+    if(ReadStream(stream,&number))
+    {
+        *pRoot=new BinaryTreeNode();
+        (*pRoot)->m_nValue=number;
+        (*pRoot)->m_pLeft=nullptr;
+        (*pRoot)->m_pRight=nullptr;
+
+        Deserialize(&((*pRoot)->m_pLeft),stream);
+        Deserialize(&((*pRoot)->m_pRight),stream);
+    }
+}
