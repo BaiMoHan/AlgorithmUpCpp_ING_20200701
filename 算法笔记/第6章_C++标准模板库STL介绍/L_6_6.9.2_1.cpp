67e234c6e55286b5d45b67693d973b40ea66d7f6
baimohan <csbaimohan@foxmail.com>
2020-07-19 22:27:40 +0800

    swap()

---
+++ 算法笔记/第6章_C++标准模板库STL介绍/L_6_6.9.2_1.cpp
@@ -0,0 +1,12 @@
+//swap(x,y)交换x和y的值
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int x=1,y=2;
+	printf("x=%d,y=%d\n",x,y);
+	swap(x,y);
+	printf("x=%d,y=%d\n",x,y);
+	return 0;
+ } 
